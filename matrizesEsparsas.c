505cb23124c6e71d5a1653244f898e7f8aa3d666
Ysis Barduco <ysisbarduco@gmail.com>
2026-04-28 22:10:31 -0300

    Alteração nas funções *Aloca_Nodo, imprime_matriz, insere_lista EXIGE CORREÇÃO!

--- matrizesEsparsas.c
+++ matrizesEsparsas.c
@@ -2,11 +2,11 @@
 // Ysis Barduco Straub de Lima
 /** 
 Trabalho I - Matrizes Esparsas
-	Oberva��es:
-	- O usu�rio deve informar a dimens�o das matrizes.
-	- Considerar as regras de opera��es com matrizes para implementar a soma, subtra��o, multiplica��o, transposta e diagonal principal de matrizes.
-	- As listas encadeadas devem ser passadas como par�metros das fun��es.
-	- O usu�rio poder� fazer quantas opera��es desejar com as matrizes
+	Obervações:
+	- O usuário deve informar a dimens�o das matrizes.
+	- Considerar as regras de opera��es com matrizes para implementar a soma, subtração, multiplicação, transposta e diagonal principal de matrizes.
+	- As listas encadeadas devem ser passadas como parametros das funções.
+	- O usuário poderia fazer quantas operações desejar com as matrizes
 **/
 
 #include <stdio.h>
@@ -18,8 +18,8 @@ typedef struct nodo{
 	struct nodo *prox;
 }Matriz_Esparsa;
 
-//Prot�tipos
-Matriz_Esparsa aloca_nodo();
+//Protótipos
+Matriz_Esparsa *aloca_nodo();
 void insere_lista(Matriz_Esparsa **m, float dado, int linha, int coluna);
 void busca_dados(Matriz_Esparsa *m);
 void libera_memoria(Matriz_Esparsa **m);
@@ -31,41 +31,42 @@ void gera_transposta(Matriz_Esparsa **m);
 void imprime_matriz(Matriz_Esparsa *m, int tot_lin, int tot_col);
 void imprime_diagonal(Matriz_Esparsa *m);
 
-//Implementa��o
+//Implementação
 
-Matriz_Esparsa aloca_nodo(){
+Matriz_Esparsa *aloca_nodo(){
 	Matriz_Esparsa *p; //Cria Nodo
 	p = (Matriz_Esparsa *) malloc(sizeof (Matriz_Esparsa)); //Aloca mem�ria
 	if(!p){
 		printf("Problema de alocacao!");
 		exit(0);
 	}
-	return *p;
+	return p;
 }
 
 void insere_lista(Matriz_Esparsa **m, float dado, int linha, int coluna){
 	Matriz_Esparsa *novo, *aux; // variaveis temporarias
-	*novo = aloca_nodo(); // chama fun��o para criar o nodo
+	novo = aloca_nodo(); // chama função para criar o nodo
 	novo->dado = dado;
 	novo->lin = linha;
 	novo->col = coluna;
 	novo->prox = NULL;
 	
 	//Insere ordenado por linha e coluna
-	if((*m)->lin >= linha || (*m)->col >= coluna || *m == NULL){
+	if(*m == NULL || (*m)->lin > linha && (*m)->col > coluna){
 		novo->prox = *m; // insere antes do primeiro valor salvo
 		*m = novo;
 		return;
 	}
 	
 	aux = *m;
-	while(aux->prox != NULL && aux->lin < linha && aux->col < coluna){
+	while(aux->prox != NULL && (aux->lin < linha || (aux->lin == linha && aux->col < coluna))){
 		aux = aux->prox; // procura a maior posição salva
 	}
 	novo->prox = aux->prox; // insere antes da posição maior
 	aux->prox = novo;
 	
 }
+
 void busca_dados(Matriz_Esparsa *m){
 	
 }
@@ -84,14 +85,14 @@ void le_matriz(Matriz_Esparsa **m, int *tot_lin, int *tot_col){
 	printf("Total de colunas: ");
 	scanf("%d", tot_col);
 	
-	printf("\nDigite os dados diferentes de 0 e suas posi��es.\n Para finalizar a leitura, digite 0.");
-   do{ // solicita a entreda do us�rio ao menos uma vez
+	printf("\nDigite os dados diferentes de 0 e suas posições.\n Para finalizar a leitura, digite 0.");
+   do{ // solicita a entrada do usuário ao menos uma vez
         printf("\nDado: ");
         scanf("%f", &dado);
          
         if(dado == 0){
              printf("\nLeitura finalizada!\n");
-             return; // garante que o dado 0 n�o seja salvo na lista
+             return; // garante que o dado 0 não seja salvo na lista
         }
          
         printf("\nLinha: ");
@@ -100,11 +101,11 @@ void le_matriz(Matriz_Esparsa **m, int *tot_lin, int *tot_col){
         printf("\nColuna: ");
         scanf("%d", &coluna);
          
-        insere_lista(m, dado, linha, coluna); // chama a fun��o para inserir o novo dado na lista
+        insere_lista(m, dado, linha, coluna); // chama a função para inserir o novo dado na lista
          
         printf("\n***\n");
 
-    }while(dado != 0); // encerra a execu��o quando o dado for 0
+    }while(dado != 0); // encerra a execução quando o dado for 0
    
 	
 }
@@ -127,24 +128,28 @@ void gera_transposta(Matriz_Esparsa **m){
 
 void imprime_matriz(Matriz_Esparsa *m, int tot_lin, int tot_col){
 	int i, j;
+	Matriz_Esparsa *aux;
 	
 	if(m == NULL){
       printf("\nMatriz vazia!\n");
       return;
       }
 	
+	aux = m;
+
 	for(i = 0; i < tot_lin; i++){ // para cada linha
-     for(j = 0; j < tot_col; j++){ // para cada coluna
-        if(m->col == j && m->lin == i){ // imprime os dados da lista em suas posi��es na matriz
-            printf("%d ", m->dado);
-        }
-        else{ // imprime os 0
-            	printf("0 ");
-        }
-     m = m->prox;
-   }
-    printf("\n"); // Mudan�a de linha
- }
+		for(j = 0; j < tot_col; j++){ // para cada coluna
+			if(aux != NULL && aux->lin == j && aux->col == i){ // imprime os dados da lista em suas posições na matriz
+				printf("%.2f ", aux->dado);
+			}
+			else{ // imprime os 0
+				printf("0.0 ");
+			}
+			m = m->prox;
+		}
+		printf("\n"); // Mudança de linha
+	}
+	
 }
 
 void imprime_diagonal(Matriz_Esparsa *m){
